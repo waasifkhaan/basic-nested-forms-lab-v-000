6fa85c37dc878f1c20b33cf4fd21f3ec4b1589bc
Wasif Khan <waasif.khaan@gmail.com>
2018-10-10 23:01:08 +0000

    automatically backed up by learn

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -9,7 +9,7 @@ class RecipesController < ApplicationController
 
   def new
     @recipe = Recipe.new
-    @recipe.ingredients.build(name: 'Cheese', quantity: "heavy duty")
+    2.times { @recipe.ingredients.build }
     
   end
 
